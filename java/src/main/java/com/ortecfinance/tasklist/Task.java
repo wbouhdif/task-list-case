734855fec0fbc26e2fecb90f93533fb0ece75cdb
Walid Bouhdif <wbouhdif@hotmail.com>
2026-03-19 20:16:24 +0100

    add deadline field and corresponding getters/setters to Task class

--- java/src/main/java/com/ortecfinance/tasklist/Task.java
+++ java/src/main/java/com/ortecfinance/tasklist/Task.java
@@ -1,14 +1,18 @@
 package com.ortecfinance.tasklist;
 
+import java.util.Date;
+
 public final class Task {
     private final long id;
     private final String description;
     private boolean done;
+    private Date deadline;
 
     public Task(long id, String description, boolean done) {
         this.id = id;
         this.description = description;
         this.done = done;
+        this.deadline = null;
     }
 
     public long getId() {
@@ -19,11 +23,19 @@ public final class Task {
         return description;
     }
 
+    public Date getDeadline() {
+        return deadline;
+    }
+
     public boolean isDone() {
         return done;
     }
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
     }
 }
